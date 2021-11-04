932db9c134cb612911647a5032524816e0087c24
Daniel Schulz <daniel@schulz.codes>
2021-11-04 20:56:00 +0100

    add react app with csb build

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import { render } from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({ config: { initialColorMode: "dark", s: true } });
+render(
+  <StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </StrictMode>,
+  document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
